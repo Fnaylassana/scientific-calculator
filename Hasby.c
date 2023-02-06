72754d310b6a27a1d0380c48324125cce8890c6f
Hasby Raihan <hasby.raihan.tif22@polban.ac.id>
2023-02-06 10:44:36 +0700

    Logaritma 10

---
+++ Hasby.c
@@ -0,0 +1,8 @@
+#include "Hasby.h"
+int Log(Angka1){
+	int Hasil;
+
+    Hasil = log10(Angka1);
+   return Hasil;
+}
+ 
---
+++ Hasby.h
@@ -0,0 +1,8 @@
+#define Hasby_H
+#ifndef Hasby_H
+#include <stdio.h>
+#include <math.h>
+
+	int Log(Angka1);
+#endif
+
