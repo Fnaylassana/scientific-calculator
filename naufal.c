4989adad3cd2aacb295e385745ffc84380dac5c2
naufal <m.naufal.tif22@polban.ac.id>
2023-02-06 10:05:49 -0800

    File Naufal penjumlahan

---
+++ naufal.c
@@ -0,0 +1,8 @@
+#include "naufal.h"
+
+int pertambahan(int nilai1, int nilai2){
+	int temp;
+	
+	temp = nilai1 + nilai2;
+	return temp;
+}
---
+++ naufal.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#ifndef naufal_H
+#define naufal_H
+#include <math.h>
+
+int pertambahan(int nilai1, int nilai2);
+
+#endif
