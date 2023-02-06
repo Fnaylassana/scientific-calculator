7821fde30a468beecd715c2a17dfc6acc0997da2
Fauza Naylassana <fauza.naylassana.tif22@polban.ac.id>
2023-02-06 11:55:15 +0700

    Update main (1) final

--- main.c
+++ main.c
@@ -1,35 +1,65 @@
 #include "Fauza.h"
+#include "naufal.h"
+#include "Hasby.h"
+#include "aulianf.h"
+#include "aryagara2.h"
 
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	int operan1, operan2;
 	int operasi;
 	int hasil1;
-	double hasil2;
-	
-	printf ("Masukkan operan pertama : ");
-	scanf  ("%d", &operan1);
+	double operan, hasil2;
+
 	
 	printf ("\nOperasi yang tersedia :");
 	printf ("\n\t1. Nilai Nutlak");
 	printf ("\n\t2. Tambah");
-	printf ("\n\t3. Eksponen");
-	printf ("\n\t4. Logaritma");
+	printf ("\n\t3. Pangkat");
+	printf ("\n\t4. Logaritma 10");
 	printf ("\n\t5. Logaritma Natural");
-	printf ("\nPilih operasi yang akan dijalankan (berupa angka):");
-	scanf  ("%d", &operasi);
 	
+	printf ("\n\nPilih operasi yang akan dijalankan (berupa angka): ");
+	scanf  ("%d", &operasi);
+
 	switch (operasi){
-		case 1 :
+		case 1 :	
+			printf ("\nMasukkan operan pertama : ");
+			scanf  ("%d", &operan1);
 			hasil1 = NilaiMutlak (operan1);
 			printf ("\nHasilnya adalah : %d", hasil1);
 			break;
 			
-		//case 2 :
-		//	hasil1 = NilaiMutlak (operan1);
-		//	printf ("\nHasilnya adalah : %d", hasil1);
+		case 2 :	
+			printf ("\nMasukkan operan pertama : ");
+			scanf  ("%d", &operan1);
+			printf ("\nMasukkan operan kedua : ");
+			scanf  ("%d", &operan2);
+			hasil1 = pertambahan(operan1, operan2);
+			printf ("\nHasilnya adalah : %d", hasil1);
+			break;
+			
+		case 3 :	
+			printf ("\nMasukkan operan pertama : ");
+			scanf  ("%d", &operan1);
+			printf ("\nMasukkan operan kedua : ");
+			scanf  ("%d", &operan2);
+			hasil1 = eksponen(operan1, operan2);
+			printf ("\nHasilnya adalah : %d", hasil1);
+			break;
+		
+		case 4 :	
+			printf ("\nMasukkan operan pertama : ");
+			scanf  ("%d", &operan1);
+			hasil1 = Log(operan1);
+			printf ("\nHasilnya adalah : %d", hasil1);
+			break;
+			
+		case 5 :	
+			printf ("\nMasukkan operan pertama : ");
+			scanf  ("%lf", &operan);
+			hasil2 = hitungLog(operan);
+			break;
 	}
 	
 	return 0;
