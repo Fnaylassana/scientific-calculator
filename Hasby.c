a7deefca443d6e6b9ac79ebcb954adeea742ecc4
Hasby Raihan <hasby.raihan.tif22@polban.ac.id>
2023-02-13 12:52:17 +0700

    Menambahjan Convert suhu dari Celcius

--- Hasby.c
+++ Hasby.c
@@ -1,8 +1,28 @@
-#include "Hasby.h"
+#include "Hasby.H"
+
 int Log(Angka1){
 	int Hasil;
 
     Hasil = log10(Angka1);
    return Hasil;
 }
+
+float ConCelFahr(float celc){
+	float fahr; 
+	fahr = (9.0/5.0 * celc) + 32;
+	return fahr;
+}
  
+ float ConCelKelv(float celc){
+	float kelv;
+	 
+	kelv = celc + 273.15;
+	return kelv;
+}
+float ConCelReam(float celc){
+	float ream;
+	 
+	ream = celc * (4.0/5.0) ;
+	return ream;
+}
+
--- Hasby.h
+++ Hasby.h
@@ -1,8 +1,12 @@
-#define Hasby_H
-#ifndef Hasby_H
 #include <stdio.h>
 #include <math.h>
+#define Hasby_H
+#ifndef Hasby_H
+
 
 	int Log(Angka1);
+	float ConCelFahr(float celc);
+	float ConCelKelv(float celc);
+	float ConCelReam(float celc);
 #endif
 
