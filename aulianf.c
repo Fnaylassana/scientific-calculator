3aa5cac1c8675f5a00ef04da4282d04c2e66e362
Aulia Nurul Fauziah <aulia.nurul.tiff22@gmail.com>
2023-02-13 13:03:05 +0700

    Trigonometri - Sin

--- aulianf.c
+++ aulianf.c
@@ -7,3 +7,20 @@ int eksponen(int bilangan1, int bilangan2)
 	hasil = pow(bilangan1,bilangan2);     	// Menghitung pangkat dengan fungsi pow 
 	return hasil;
 }
+
+double sinRad(double angka)
+// Menghitung sin berupa radian
+{
+	double rad;
+	rad = cos(angka); 
+	return rad;
+}
+
+double sinDerajat(double angka)
+// Menghitung sin berupa derajat
+{
+	double nilai, derajat;
+	nilai = pi/180;
+	derajat = sin(angka*nilai);
+	return derajat;
+}
--- aulianf.h
+++ aulianf.h
@@ -5,5 +5,9 @@
 
 // Menghitung bilangan berpangkat
 int eksponen(int bilangan1, int bilangan2);
+// Menghitung sin berupa radian
+double sinRad(double angka);
+// Menghitung sin berupa derajat
+double sinDerajat(double angka);
 
 #endif
