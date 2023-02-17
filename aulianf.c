0cc9f218063d6a6bdafbd47c08160a25ee4ebb0e
Aulia Nurul Fauziah <aulia.nurul.tiff22@gmail.com>
2023-02-17 18:13:47 +0700

    Akar dan Sin

--- aulianf.c
+++ aulianf.c
@@ -1,16 +1,92 @@
-double sinRad(double angka)
-// hasil sin berupa radian
+#include "aulianf.h"
+#define pi 3.14
+
+/* float eksponen(float bilangan1, float bilangan2)
+// Menghitung bilangan berpangkat
 {
-	double rad;
-	rad = cos(angka); 
+	float hasil1;							
+	hasil1 = pow(bilangan1,bilangan2);     	// Menghitung pangkat dengan fungsi pow 
+	return hasil1;
+} */
+
+float eksponen (float bilangan1, float bilangan2)
+{   
+	float hasil;
+	hasil = 1;
+        
+    if(bilangan2 > 0){
+		while (bilangan2 != 0)
+	    {
+	        hasil = hasil * bilangan1;
+	        bilangan2 = bilangan2 - 1;
+	    } 
+    } 
+    
+    if(bilangan2 < 0){
+		while (bilangan2 != 0)
+	    {
+	        hasil = hasil * bilangan1;
+	        bilangan2 = bilangan2 + 1;
+	    } 
+        hasil = 1/hasil;
+    } 
+    
+    return hasil;
+} 
+
+float akar (float bilangan)
+// Menghitung akar
+{
+	float hasil;
+	while (bilangan < 0){
+		printf("Hasil tidak terdefinisi, silahkan masukkan kembali bilangan");
+		printf("\n\n2Masukkan bilangan : ");
+		scanf("%f",&bilangan);
+	}
+	hasil = sqrt(bilangan);		// Menghitung akar dengan fungsi sqrt		
+	return hasil;
+}
+
+float sinRad(float bilangan)
+// Hasil sin berupa radian
+{
+	float rad;
+	rad = sin(bilangan); 
 	return rad;
 }
 
-double sinDerajat(double angka)
-// hasil sin berupa derajat
+float sinDerajat(float bilangan)
+// Hasil sin berupa derajat
 {
-	double nilai, derajat;
+	float nilai, derajat;
 	nilai = pi/180;
-	derajat = sin(angka*nilai);
+	derajat = sin(bilangan*nilai);
 	return derajat;
 }
+
+float sinInversRad(float bilangan)
+// Hasil Invers berupa derajat
+{
+	float rad;
+	while (bilangan < -1 || bilangan > 1){
+	printf("Silahkan masukkan kembali bilangan (-1 - 1)");
+		printf("\n\n2Masukkan bilangan : ");
+		scanf("%f",&bilangan);
+	} 
+	rad = asin(bilangan);
+	return rad;
+} 
+
+float sinInversDerajat(float bilangan)
+// Hasil Invers berupa derajat
+{
+	float nilai, derajat;
+	while (bilangan < -1 || bilangan > 1){
+	printf("Silahkan masukkan kembali bilangan (-1 - 1)");
+		printf("\n\n2Masukkan bilangan : ");
+		scanf("%f",&bilangan);
+	} 
+	nilai = 180/pi;
+	derajat = ((asin(bilangan))*nilai);
+	return derajat;
+} 
--- aulianf.h
+++ aulianf.h
@@ -1,9 +1,24 @@
+#ifndef aulianf_h
+#define aulianf_h
 #include <stdio.h>
 #include <math.h>
-#define AuliaNF_H
-#ifndef AuliaNF_H
 
 // Menghitung bilangan berpangkat
-double eksponen(int bilangan1, int bilangan2);
+float eksponen (float bilangan1, float bilangan2);
+
+// Menghitung akar
+float akar(float bilangan);
+
+// Menghitung sin dalam radian
+float sinRad(float angka);
+
+// Menghitung sin dalam derajat
+float sinDerajat(float angka);
+
+// Menghitung invers sin dalam radian
+float sinInversRad(float bilangan);
+
+// Menghitung invers sin dalam derajat
+float sinInversDerajat(float bilangan);
 
 #endif
