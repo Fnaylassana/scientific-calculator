ed9a7193d8e45415d5b4bcbe1c341bc9aefd3267
naufal <m.naufal.tif22@polban.ac.id>
2023-02-18 14:09:19 -0800

    perbaikan modul kombinasi dan permutasi

--- naufal.h
+++ naufal.h
@@ -9,7 +9,7 @@ float perkalian(float nilai1, float nilai2);
 float pembagian(float nilai1, float nilai2);
 int faktorial(int temp);
 int modulus(int nilai1, int nilai2);
-int kombinasi(int nilai1, int nilai2, int hasil);
-int permutasi(int nilai1, int nilai2, int hasil);
+int kombinasi(int nilai1, int nilai2);
+int permutasi(int nilai1, int nilai2);
 
 #endif
