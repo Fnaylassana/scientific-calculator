c3fdc276eceeed82cd30e5106980b68415325f10
naufal <m.naufal.tif22@polban.ac.id>
2023-02-17 17:01:11 -0800

    `

--- naufal.h
+++ naufal.h
@@ -7,5 +7,9 @@ float pertambahan(float nilai1, float nilai2);
 float pengurangan(float nilai1, float nilai2);
 float perkalian(float nilai1, float nilai2);
 float pembagian(float nilai1, float nilai2);
+int faktorial(int temp);
+int modulus(int nilai1, int nilai2);
+int kombinasi(int nilai1, int nilai2, int hasil);
+int permutasi(int nilai1, int nilai2, int hasil);
 
 #endif
