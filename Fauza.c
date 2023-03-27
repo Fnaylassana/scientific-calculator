48717de6c2f0ebf06ff052c9a81bc90c48528547
Fauza Naylassana <fauza.naylassana.tif22@polban.ac.id>
2023-03-27 11:11:41 +0700

    menambahkan spesifier jika eror

--- Fauza.c
+++ Fauza.c
@@ -70,7 +70,11 @@ int isNumber(address token)
 {
 	char c;
 	c = *Info(token);
-    return (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || negativeInteger(token));
+	if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || negativeInteger(token)){
+		return 1;
+	} else{
+		return 0;
+	}
 }
 
 int isOperator(char c)
@@ -397,6 +401,8 @@ double hitungIsiPostfix(List postfix, int *cek)
     {
         if(isNumber(token))
         {
+        	printf ("F\n");
+        	getche();
             Push(&stack, atof(Info(token)));
         }
         else if(isOperator(*Info(token)))
