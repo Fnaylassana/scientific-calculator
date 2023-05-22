cbd8b3fefcfbd21e89ea798b13074312f02b0c9b
naufal <m.naufal.tif22@polban.ac.id>
2023-05-22 10:47:38 +0700

    comment

--- naufal.h
+++ naufal.h
@@ -2,45 +2,87 @@
 #ifndef naufal_H
 #define naufal_H
 #define max 10
-//fungsi pertambahan
-//referensi : Algoritma & pemrograman : Andri Kristanto
+
+/**
+ * fungsi pertambahan
+ * referensi 	: Algoritma & pemrograman : Andri Kristanto
+ * IS			: Belum tampil hasil nilai dari kedua operand yang akan dilakukan operasi pertamabahan
+ * FS			: Sudah tampil hasil nilai dari kedua operand yang akan dilakukan operasi pertamabahan
+ */
 double pertambahan(double nilai1, double nilai2);
 
-// fungsi pengurangan
-//referensi : Algoritma & pemrograman : Andri Kristanto
+/**
+ * fungsi pengurangan
+ * referensi 	: Algoritma & pemrograman : Andri Kristanto
+ * IS			: Belum tampil hasil nilai dari kedua operand yang akan dilakukan operasi pengurangan
+ * FS			: Sudah tampil hasil nilai dari kedua operand yang akan dilakukan operasi pengurangan
+ */
 double pengurangan(double nilai1, double nilai2);
 
-//fungsi perkalian
-//referensi : Algoritma & pemrograman : Andri Kristanto
+/**
+ * fungsi perkalian
+ * referensi 	: Algoritma & pemrograman : Andri Kristanto
+ * IS			: Belum tampil hasil nilai dari kedua operand yang akan dilakukan operasi perkalian
+ * FS			: Sudah tampil hasil nilai dari kedua operand yang akan dilakukan operasi perkalian
+ */
+
 double perkalian(double nilai1, double nilai2);
 
-//fungsi pembagian
-//referensi : Algoritma & pemrograman : Andri Kristanto
+/**
+ * fungsi pembagian
+ * referensi 	: Algoritma & pemrograman : Andri Kristanto
+ * IS			: Belum tampil hasil nilai dari kedua operand yang akan dilakukan operasi pembagian
+ * FS			: Sudah tampil hasil nilai dari kedua operand yang akan dilakukan operasi pembagian
+ */
 double pembagian(double nilai1, double nilai2);
 
-//fungsi faktorial
-//referensi : duniailkom.com
+/**
+ * fungsi faktorial
+ * referensi 	: duniailkom.com
+ * IS			: Belum tampil hasil nilai dari satu operand yang akan dilakukan operasi faktorial
+ * FS			: Sudah tampil hasil nilai dari satu operand yang akan dilakukan operasi faktorial
+ */
 double faktorial(double angka);
 
-//fungsi modulus
-//referensi : geeksforgeeks.org
+/**
+ * fungsi modulus
+ * referensi 	: geejsforgeeks.org
+ * IS			: Belum tampil hasil nilai dari kedua operand yang akan dilakukan operasi modulus
+ * FS			: Sudah tampil hasil nilai dari kedua operand yang akan dilakukan operasi modulus
+ */
 int modulus(int nilai1, int nilai2);
 
-//fungsi kombinasi
-//referensi : ngodingdirumah.home.blog/2019/04/30/membuat-program-permutasi-dan-kombinasi-dalam-bahasa-c/
+/**
+ * fungsi kombinasi
+ * referensi 	: ngodingdirumah.home.blog/2019/04/30/membuat-program-permutasi-dan-kombinasi-dalam-bahasa-c/
+ * IS			: Belum tampil hasil nilai dari kedua operand yang akan dilakukan opersi kombinasi
+ * FS			: Sudah tampil hasil nilai dari kedua operand yang akan dilakukan opersi kombinasi
+ */
 double kombinasi(double nilai1, double nilai2);
 
-//fungsi permutasi
-//referensi : ngodingdirumah.home.blog/2019/04/30/membuat-program-permutasi-dan-kombinasi-dalam-bahasa-c/
+/**
+ * fungsi permutasi
+ * referensi 	: ngodingdirumah.home.blog/2019/04/30/membuat-program-permutasi-dan-kombinasi-dalam-bahasa-c/
+ * IS			: Belum tampil hasil nilai dari kedua operand yang akan dilakukan opersi permutasi
+ * FS			: Sudah tampil hasil nilai dari kedua operand yang akan dilakukan opersi permutasi
+ */
 double permutasi(double nilai1, double nilai2);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//prosedur transpose matriks
-//referensi : Fauza Naylasana
+/**
+ * prosedur transpose matriks
+ * kontributor  : Fauza Naylasana(010)
+ * IS			: Belum tampil hasil nilai dari kedua operand yang akan dilakukan opersi transpose matriks
+ * FS			: Sudah tampil hasil nilai dari kedua operand yang akan dilakukan opersi transpose matriks
+ */
 void transposeMatriks (double matriks[max][max], double transpose[max][max], int baris, int kolom);
 
-//prosedur help untuk pemanggilan tampilan pembantu
+/**
+ * prosedur help untuk pemanggilan tampilan pembantu
+ * IS			: Belum ditampilkan tampilan pembantu
+ * FS			: Sudah ditampilkan tampilan pembantu
+ */
 void help();
 #endif
